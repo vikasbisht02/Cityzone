32bbb640712aaf2d83c08da0fb7acaa648c778bd
Vikas Singh Bisht <vikasbisht732002@gmail.com>
2026-04-06 07:37:16 +0530

    Added helper function

--- Citizone-backend/src/utils/password.js
+++ Citizone-backend/src/utils/password.js
@@ -1,10 +1,16 @@
 import bcrypt from "bcryptjs";
 
 export const hashPassword = async (password) => {
+  if(!password) return false;
   const salt = await bcrypt.genSalt(12);
   return await bcrypt.hash(password, salt);
 };
 
 export const comparePassword = async (plainPassword, hashedPassword) => {
+  if(!plainPassword || !hashPassword) return false;
   return await bcrypt.compare(plainPassword, hashedPassword);
 };
+
+export const isPasswordSame = (password, confirmPassword) => {
+  return password === confirmPassword;
+}
