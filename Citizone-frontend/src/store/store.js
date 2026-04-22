e2cac9e204ccc13a5328bf253a5690b3f2d23d33
Vikas Singh Bisht <vikasbisht732002@gmail.com>
2026-04-22 22:38:35 +0530

    Deleted duplicate store

--- Citizone-frontend/src/store/store.js
+++
@@ -1,7 +0,0 @@
-// redux/store.js
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "../reducer/rootReducer";
-
-export const store = configureStore({
-  reducer: rootReducer,
-});
